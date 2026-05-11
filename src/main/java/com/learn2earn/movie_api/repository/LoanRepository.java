fc07ddf9405832fe3539a4e07d43606d4d495b9d
kostas900402 <nascoskostas@gmail.com>
2026-05-11 10:32:47 +0200

    loanrepo, ändrade andra repo till long

--- src/main/java/com/learn2earn/movie_api/repository/LoanRepository.java
+++ src/main/java/com/learn2earn/movie_api/repository/LoanRepository.java
@@ -1,9 +1,9 @@
 package com.learn2earn.movie_api.repository;
 
-import com.learn2earn.movie_api.model.Director;
+import com.learn2earn.movie_api.model.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DirectorRepository extends JpaRepository<Director, Long> {
+public interface LoanRepository extends JpaRepository<Loan, Long> {
 }
--- src/main/java/com/learn2earn/movie_api/repository/MovieRepository.java
+++ src/main/java/com/learn2earn/movie_api/repository/MovieRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.*;
 
 @Repository
-public interface MovieRepository extends JpaRepository<Movie, String>{
+public interface MovieRepository extends JpaRepository<Movie, Long>{
 
 }
 
